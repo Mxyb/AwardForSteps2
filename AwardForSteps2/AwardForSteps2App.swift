4739dc4013a7b3f90ffdd22214ffcfb10d64864c
Mxyb <97235899+Mxyb@users.noreply.github.com>
2022-02-25 16:20:04 +0000

    Initial Commit

---
+++ AwardForSteps2/AwardForSteps2App.swift
@@ -0,0 +1,17 @@
+//
+//  AwardForSteps2App.swift
+//  AwardForSteps2
+//
+//  Created by Max Baker on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AwardForSteps2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
